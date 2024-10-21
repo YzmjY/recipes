00571ec986c8e8f9182ed8931f6161b58c787370
zhangmingjie <zhangmingjie.xzmjx@bytedance.com>
2024-10-21 19:56:18 +0800

    增加单调栈相关

---
+++ note/刷题/stack.md
@@ -0,0 +1,74 @@
+# Stack
+
+## 相关算法
+### 单调栈
+单调栈：栈内元素单调递增或递减，通常用来查找左侧或右侧第一个更大或更小的元素。
+#### 左侧
+用单调栈来解决查找左侧元素问题时，关注点应该在栈顶元素与次顶元素。
+- 查找左侧第一个更大元素：
+```cpp
+vector<int> stackAlgo(const vector<int>& arr) {
+	stack<int> stk;
+	vector<int> res(arr.size(), -1);
+
+	for (int i = 0; i < arr.size(); ++i) {
+		// 递减单调栈
+		while (!stk.empty() && arr[stk.top()] <= arr[i]) {
+			int top = stk.top();
+			stk.pop();
+			if (stk.empty()) {
+				break;
+			}
+			int temp = stk.top();
+			// top的左侧第一个比top大的元素
+			res[top] = arr[temp];
+		}
+		stk.push(i);
+	}
+	while (!stk.empty()) {
+		int top = stk.top();
+		stk.pop();
+		if (stk.empty()) {
+			break;
+		}
+		int temp = stk.top();
+		res[top] = arr[temp];
+	}
+	return res;
+}
+```
+- 查找左侧第一个更小元素：
+```cpp
+vector<int> stackAlgo(const vector<int>& arr) {
+	stack<int> stk;
+	vector<int> res(arr.size(), -1);
+	print("init", arr);
+
+	for (int i = 0; i < arr.size(); ++i) {
+		// 递增单调栈, 等号也应该处理，因为i一定是更大的
+		while (!stk.empty() && arr[stk.top()] >= arr[i]) {
+			int top = stk.top();
+			stk.pop();
+			if (stk.empty()) {
+				break;
+			}
+			int temp = stk.top();
+			// top的左侧第一个比top小的元素
+			res[top] = arr[temp];
+		}
+		stk.push(i);
+	}
+	while (!stk.empty()) {
+		int top = stk.top();
+		stk.pop();
+		if (stk.empty()) {
+			break;
+		}
+		int temp = stk.top();
+		res[top] = arr[temp];
+	}
+	return res;
+}
+```
+#### 右侧
+用单调栈来解决查找右侧元素问题时，关注点应该在栈顶元素与待入栈元素。
--- note/刷题/test.cpp
+++ note/刷题/test.cpp
@@ -1,31 +1,70 @@
 #include <iostream>
+#include <stack>
 #include <vector>
 using namespace std;
-void sort(vector<int>& arr)
-{
-    int n = arr.size();
-    // 写一个插入排序
-    for (int i = 1; i < n; ++i) {
-        int cur = arr[i];
-        for (int j = i - 1; j >= 0; --j) {
-            if (cur < arr[j]) {
-                arr[j + 1] = arr[j];
-            } else {
-                arr[j + 1] = cur;
-                break;
-            }
-        }
-    }    
+
+void print(const string& prefix, const vector<int>& arr) {
+	cout << prefix << ": [";
+	for (int i = 0; i < arr.size(); ++i) {
+		cout << arr[i];
+		if (i != arr.size() - 1) {
+			cout << ", ";
+		}
+	}
+	cout << "]";
+	cout << endl;
 }
 
-int main()
-{
-    vector<int> arr = {5, 2, 4, 6, 1, 3};
-    sort(arr);
-    for (int i = 0; i < arr.size(); ++i)
-    {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
-    return 0;
+void sort(vector<int>& arr) {
+	int n = arr.size();
+	// 写一个插入排序
+	for (int i = 1; i < n; ++i) {
+		int cur = arr[i];
+		for (int j = i - 1; j >= 0; --j) {
+			if (cur < arr[j]) {
+				arr[j + 1] = arr[j];
+			} else {
+				arr[j + 1] = cur;
+				break;
+			}
+		}
+	}
+}
+
+vector<int> stackAlgo(const vector<int>& arr) {
+	stack<int> stk;
+	vector<int> res(arr.size(), -1);
+	print("init", arr);
+
+	for (int i = 0; i < arr.size(); ++i) {
+		// 递增单调栈, 等号也应该处理，因为i一定是更大的
+		while (!stk.empty() && arr[stk.top()] >= arr[i]) {
+			int top = stk.top();
+			stk.pop();
+			if (stk.empty()) {
+				break;
+			}
+			int temp = stk.top();
+			// top的左侧第一个比top小的元素
+			res[top] = arr[temp];
+		}
+		stk.push(i);
+	}
+	while (!stk.empty()) {
+		int top = stk.top();
+		stk.pop();
+		if (stk.empty()) {
+			break;
+		}
+		int temp = stk.top();
+		res[top] = arr[temp];
+	}
+	return res;
+}
+
+int main() {
+	vector<int> arr = {5, 2, 4, 6, 1, 3};
+	vector<int> res = stackAlgo(arr);
+	print("res", res);
+	return 0;
 }
